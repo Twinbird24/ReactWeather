75285bc364736a9be5af9a19e1da99d5c5f8cd12
Attila Komaromi <attila.kom@gmail.com>
2017-01-14 12:27:19 -0500

    Updated middleware for localhost

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    // req.headers obj will be either http or https, if it's http we will continue normal
-    // our weatherapi can only handle http, so we need to make sure to use that
-    next();
-  } else {
-    // otherwise redirect to the http version of url
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    // req.headers obj will be either http or https, if it's https we will redirect to the http version of url
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    // otherwise we are using http so just continue normally
+    next();
   }
-})
+});
 
 app.use(express.static('public'));
 
