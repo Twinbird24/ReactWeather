577adde11020008202c03beab3dc2c1ea005716d
Attila Komaromi <attila.kom@gmail.com>
2017-01-14 12:10:17 -0500

    Prepared repo for Heroku deploy

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    // req.headers obj will be either http or https, if it's http we will continue normal
+    // our weatherapi can only handle http, so we need to make sure to use that
+    next();
+  } else {
+    // otherwise redirect to the http version of url
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
