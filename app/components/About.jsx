c922cf58e43440b43f65b562bf546f8536a8dc70
Attila Komaromi <attila.kom@gmail.com>
2017-01-16 11:06:08 -0500

    Updated readme and about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,7 +6,7 @@ var About = (props) => {
       <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built using React. The page navigation at the top is a separate React component that incorporates
       the React Router to seamlessly jump between pages, each of which is also a separate component. To fetch the weather, axios is used.
-      Axios is a promise based HTTP client for the browser that is used to grab the weather API (in JSON format) which we than handle in
+      Axios is a promise based HTTP client for the browser that is used to grab the weather API (in JSON format) which we then handle in
       the weather component (in the Get Weather page).</p>
       <p>
         Here is more information about some of the tools used in this app:
